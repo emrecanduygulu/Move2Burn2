8aad27cba458c44599446b1a9325dbf021bca6c4
Emre Can Duygulu <ecduygulu@gmail.com>
2023-12-17 00:40:32 +0100

    Initial Commit

---
+++ Move2Burn2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Move2Burn2
+//
+//  Created by emre can duygulu on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Move2Burn2/Move2Burn2App.swift
@@ -0,0 +1,17 @@
+//
+//  Move2Burn2App.swift
+//  Move2Burn2
+//
+//  Created by emre can duygulu on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Move2Burn2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
